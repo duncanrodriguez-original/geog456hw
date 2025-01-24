001fa7936dae8b76a1852daf23137db65b8c6871
duncanrodriguez-original <72842975+duncanrodriguez-original@users.noreply.github.com>
2025-01-24 00:08:02 -0500

    Add files via upload

---
+++ my_common.js
@@ -0,0 +1,8 @@
+var my_common= {
+"type": "FeatureCollection",
+"name": "my_common",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -71.160055835974219, 42.709430754103302 ] } }
+]
+}
